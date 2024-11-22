be68ccf6c8ae93418466c8da1896875cd5c5a9f8
HyunRyung <93234760+SulHyunRyung@users.noreply.github.com>
2024-11-22 16:28:41 +0900

    Guide Update

--- layerPopup/layerPopup.js
+++ layerPopup/layerPopup.js
@@ -1,6 +1,10 @@
 
 $(function(){
+    // $("element").click(function(){
+    //     $("#layer_container").css({"display":"inline-block"});
+    // });
+
     $(".ok_btn").click(function(){
-        $(this).parents('#layer_contanier').hide();
+        $(this).parents('#layer_container').hide();
     });
 });
